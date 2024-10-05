956b4ac3a4f706db7bf6e86d2711a325a2f7f09e
vladaliho <vlada.liho@gmail.com>
2024-10-05 23:33:43 +0100

    feat: add func check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const bracketsMap = {};
+  const sameBrackets = new Set();
+
+  for (let [open, close] of bracketsConfig) {
+    bracketsMap[close] = open;
+    if (open === close) {
+      sameBrackets.add(open); 
+    }
+  }
+
+  for (let char of str) {
+    if (sameBrackets.has(char)) {
+  
+      if (stack.length > 0 && stack[stack.length - 1] === char) {
+        stack.pop(); 
+      } else {
+        stack.push(char);  
+      }
+    } else if (bracketsMap[char]) {
+ 
+      if (stack.length === 0 || stack.pop() !== bracketsMap[char]) {
+        return false;  
+      }
+    } else {
+
+      stack.push(char);
+    }
+}
 }
