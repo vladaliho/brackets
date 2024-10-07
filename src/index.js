4b8fbc394f191024bc0c9da999519494cd10a093
vladaliho <vlada.liho@gmail.com>
2024-10-07 15:55:21 +0100

    fix: add fixes to func

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  const stack = [];
-  const bracketsMap = {};
-  const sameBrackets = new Set();
+    const stack = [];
+    const bracketsMap = {};
+    const openBrackets = new Set();
+    const closeBrackets = new Set();
 
-  for (let [open, close] of bracketsConfig) {
-    bracketsMap[close] = open;
-    if (open === close) {
-      sameBrackets.add(open); 
+    for (let [open, close] of bracketsConfig) {
+      bracketsMap[close] = open;
+      openBrackets.add(open);
+      closeBrackets.add(close);
     }
+
+    for (let char of str) {
+      if (openBrackets.has(char)) {
+        if (bracketsMap[char] === char) {
+          if (stack.length > 0 && stack[stack.length - 1] === char) {
+            stack.pop();
+          } else {
+            stack.push(char);
+          }
+        } else {
+          stack.push(char);
+        }
+      } else if (closeBrackets.has(char)) {
+        if (stack.length === 0 || stack.pop() !== bracketsMap[char]) {
+          return false; 
+        }
+      }
+    }
+
+    return stack.length === 0;
   }
-
-  for (let char of str) {
-    if (sameBrackets.has(char)) {
   
-      if (stack.length > 0 && stack[stack.length - 1] === char) {
-        stack.pop(); 
-      } else {
-        stack.push(char);  
-      }
-    } else if (bracketsMap[char]) {
- 
-      if (stack.length === 0 || stack.pop() !== bracketsMap[char]) {
-        return false;  
-      }
-    } else {
-
-      stack.push(char);
-    }
-}
-}
